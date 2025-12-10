e38e9a8e0cf586474d500adda971a4118e418601
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2025-12-10 22:58:57 +0700

    feat: insert 3 employers and 1 converstions

---
+++ 06-create-conversations-table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE conversations (
+  user_name VARCHAR(100),
+  employer_name VARCHAR(200),
+  user_message TEXT,
+  date_sent TIMESTAMP
+);
---
+++ 07-insert-data.sql
@@ -0,0 +1,13 @@
+-- INSERT INTO employers (company_name, company_address, yearly_revenue, is_hiring)
+--VALUES('PT SIMNU', 'Jl Buntu', 199.55, TRUE);
+
+--INSERT INTO employers (company_name, company_address, yearly_revenue, is_hiring)
+--VALUES('PT LYG', 'Jl Kasokandel, Majalengka', 100.44, FALSE);
+
+--INSERT INTO employers (company_name, company_address, yearly_revenue, is_hiring)
+--VALUES('PT SHOETOWN ', 'Jl Buntu Selatan', 200.11, TRUE);
+
+
+
+INSERT INTO conversations (user_name, employer_name, user_message, date_sent) 
+VALUES('Jin Kazama', 'PT SIMNU', 'Adili jokowi', '2025-12-10 22:57:10')
