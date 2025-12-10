bc6520dc760a4b8234ef21e488e4a17f72147181
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2025-12-10 20:58:32 +0700

    feat: inserting data to table and make documentation abouet INSERT

---
+++ 03-insert-user-data.sql
@@ -0,0 +1,8 @@
+INSERT INTO users (yearly_salary, full_name, current_status)
+ VALUES (19000, 'Burhanudin Rabbani', 'self-employed');
+
+INSERT INTO users (yearly_salary, full_name, current_status)
+ VALUES (0, 'Anies Baswedan', 'unemployed');
+
+INSERT INTO users (yearly_salary, full_name, current_status)
+ VALUES (10000, 'Jin Kazama', 'employed');
