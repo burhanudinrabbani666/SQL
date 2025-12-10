9486148a91896f88c96ee6bec4bcbcef3994a358
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2025-12-10 22:27:06 +0700

    feat: documntation about float

---
+++ 05-create-employers-table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE employers(
+  company_name VARCHAR(100),
+  company_address VARCHAR(300),
+  -- yearly_revenue FLOAT(10,10) -- Approxi
+  yearly_revenue NUMERIC(5,2), -- Exact Value
+  is_hiring BOOLEAN
+);
+
